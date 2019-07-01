1246d0579b2b84ec5b331846f014555835541f01
Huong <huongnguyenak96@gmail.com>
2019-07-01 21:28:22 +0700

    User

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: String,
+    name: String,
+    posts: [
+        { type: Schema.Types.ObjectId, ref: 'post' }
+        // post: collection name
+    ],
+    friends: [
+        { type: Schema.Types.ObjectId, ref: 'user' }
+    ],
+    receiveRequests: [
+        { type: Schema.Types.ObjectId, ref: 'user' }
+    ],
+    sendRequests: [
+        { type: Schema.Types.ObjectId, ref: 'user' }
+    ]
+})
+const UserModel = mongoose.model('user', UserSchema);
+module.exports = { UserModel }
