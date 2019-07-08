cf75ab425c1f491964f70aa51cda4b20ecf3d479
Huong <huongnguyenak96@gmail.com>
2019-07-08 19:51:32 +0700

    save

--- index.js
+++ index.js
@@ -4,52 +4,55 @@ const { PostModel } = require('./models/post.model');
 const { CommentModel } = require('./models/comment.model');
 const { hash } = require('./lib/bcrypt');
 const mongoose = require('mongoose');
+
+
+
 //4.3
 // post id: 5d1cbca406dc5c2f6a1a707d // post 5
 // user id: 5d1cb8c16b54162e91ae7e7c // user 1
 
-PostModel.findById('5d1cbca406dc5c2f6a1a707d')
-.then(async (post)=>{
-    const comment = await CommentModel.create({
-        author: '5d1cb8c16b54162e91ae7e7c',
-        post: post._id,
-        content: 'user 1 comment on post 5 (user 1)'
-    })
-    return { post, comment }
-})
-.then(obj =>{
-    return PostModel.findByIdAndUpdate(obj.post._id, {
-        $addToSet : { comments: obj.comment._id },
-    }, { new: true })
-})
-.then(post => console.log(post))
-.catch(err=>console.log(err.message))
+// PostModel.findById('5d1cbca406dc5c2f6a1a707d')
+// .then(async (post)=>{
+//     const comment = await CommentModel.create({
+//         author: '5d1cb8c16b54162e91ae7e7c',
+//         post: post._id,
+//         content: 'user 1 comment on post 5 (user 1)'
+//     })
+//     return { post, comment }
+// })
+// .then(obj =>{
+//     return PostModel.findByIdAndUpdate(obj.post._id, {
+//         $addToSet : { comments: obj.comment._id },
+//     }, { new: true })
+// })
+// .then(post => console.log(post))
+// .catch(err=>console.log(err.message))
 
 
-async function test(){
+// async function test(){
 
-    const session = await CommentModel.startSession();
-    session.startTransaction();
+//     const session = await CommentModel.startSession();
+//     session.startTransaction();
    
-    const comment = await new CommentModel({
-        author: '5d1f4f582b5f832a01af51e7',
-        post: '5d1cbca406dc5c2f6a1a707d',
-        content: 'user 2 comment on post 5 (user 1)'
-    }).save({session})
-    const post = await PostModel.findByIdAndUpdate('5d1cbca406dc5c2f6a1a707d',{
-        $addToSet : { comments: comment._id },
-    }, { new: true, session })
-    if(!post) {
-        await session.abortTransaction();
-        session.endSession();
-        console.log("rollback");
-    }
-    else {
-        await session.commitTransaction();
-        session.endSession();
-        console.log({ post })
-    }
-}
+//     const comment = await new CommentModel({
+//         author: '5d1f4f582b5f832a01af51e7',
+//         post: '5d1cbca406dc5c2f6a1a707d',
+//         content: 'user 2 comment on post 5 (user 1)'
+//     }).save({session})
+//     const post = await PostModel.findByIdAndUpdate('5d1cbca406dc5c2f6a1a707d',{
+//         $addToSet : { comments: comment._id },
+//     }, { new: true, session })
+//     if(!post) {
+//         await session.abortTransaction();
+//         session.endSession();
+//         console.log("rollback");
+//     }
+//     else {
+//         await session.commitTransaction();
+//         session.endSession();
+//         console.log({ post })
+//     }
+// }
 // test()
 
 
